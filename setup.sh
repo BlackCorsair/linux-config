f723dc60e9f615fe9eb5cf6f6abeba07b0e0693d
Jorge Hevia <jmhev@outlook.com>
2020-02-28 14:58:27 +0000

    Fix install_docker

--- setup.sh
+++ setup.sh
@@ -104,7 +104,7 @@ install_minikube(){
 install_docker(){
 	# from https://docs.docker.com/install/linux/docker-ce/ubuntu/
 	if [ "apt" == $PKG_MGR ]; then
-		sudo apt-get remove docker docker-engine docker.io containerd runc
+		sudo apt-get remove -y docker docker-engine docker.io containerd runc
 		sudo apt-get update
 		sudo apt-get install -y \
                         apt-transport-https \
@@ -114,7 +114,7 @@ install_docker(){
                         software-properties-common
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 		sudo add-apt-repository \
-			"deb [arch=amd64] https://download.docker.com/linux/debian \
+			"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    			$(lsb_release -cs) \
    			stable"
 		sudo apt-get update
